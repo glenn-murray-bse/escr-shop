be1695aa8a1683a77579f68b228b5008d0bc1a25
sestov <vsesto@odecee.com.au>
2016-06-10 14:47:27 +1000

    Changing script to get deployment running

--- firebase_deploy.sh
+++ firebase_deploy.sh
@@ -7,14 +7,14 @@ FIREBASE_TOKEN_VINCE=$2
 BRANCH=`git branch | sed -n -e 's/^\* \(.*\)/\1/p'`
 
 # Test the branch and deploy where needed
-if [[ ${BRANCH} == *"feature"* ]]
+if [[ ${TRAVIS_BRANCH} == "develop" ]]
 then
 	echo "Feature branch does not get deployed to firebase"
-elif [[ ${BRANCH} == *"develop"* ]]
+elif [[ ${TRAVIS_BRANCH} == "feature" ]]
 then
 	echo "Deploy to dev firebase environment"
 	firebase deploy --project "${FIREBASE_PROJECT_VINCE}" --token "${FIREBASE_TOKEN_VINCE}"
-elif [[ ${BRANCH} == *"master"* ]]
+elif [[ ${TRAVIS_BRANCH} == *"master"* ]]
 then
 	echo "Master branch deploy to production"
 	firebase deploy --project "${FIREBASE_PROJECT_PROD}" --token "${FIREBASE_TOKEN_PROD}"
