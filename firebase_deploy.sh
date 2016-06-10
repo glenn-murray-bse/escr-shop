6a02b786e896914c723fb1a0ff10721b1a4cc2fc
sestov <vsesto@odecee.com.au>
2016-06-10 14:56:39 +1000

    Not sure what else to try

--- firebase_deploy.sh
+++ firebase_deploy.sh
@@ -5,12 +5,13 @@ FIREBASE_TOKEN_VINCE=$2
 #FIREBASE_PROJECT_VINCE="escr-shop-dev"
 #FIREBASE_TOKEN_VINCE="1/QGZ73kIZI9BbP17zM_xkha5XWsuJqhXx-nHAYqj3rDE"
 BRANCH=`git branch | sed -n -e 's/^\* \(.*\)/\1/p'`
+TRAVIS_BRANCH=$3
 
 # Test the branch and deploy where needed
-if [[ ${TRAVIS_BRANCH} == "develop" ]]
+if [[ ${TRAVIS_BRANCH} == "feature" ]]
 then
 	echo "Feature branch does not get deployed to firebase"
-elif [[ ${TRAVIS_BRANCH} == "feature" ]]
+elif [[ ${TRAVIS_BRANCH} == "develop" ]]
 then
 	echo "Deploy to dev firebase environment"
 	firebase deploy --project "${FIREBASE_PROJECT_VINCE}" --token "${FIREBASE_TOKEN_VINCE}"
