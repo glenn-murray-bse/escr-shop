5df2189748e74f23208a061cc64661448209abac
Priyanka Jagga <pjagga@Priyankas-MBP.gateway>
2016-06-12 23:29:34 +1000

    added page elements

--- specs/steps/step-definition/modal.js
+++ specs/steps/step-definition/modal.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
-var chai = require('chai');
-var chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
+//var chai = require('chai');
+//var chaiAsPromised = require('chai-as-promised');
+//chai.use(chaiAsPromised);
 var expect = chai.expect;
 
 var modal = require('./../pageElements/modal.js');
@@ -26,68 +26,4 @@ module.exports = function() {
             console.log('dialog is not Visible');
         }
     });
-    //this.When(/^I close the dialog$/, function(callback) {
-    //    dialog.getCloseIcon().click().should.notify(callback);
-    //});
-    //
-    //this.When(/^I click the x close dialog button$/, function(callback) {
-    //    dialog.getCloseIcon().click().should.notify(callback);
-    //});
-    //
-
-    //
-    //this.Then(/^the "(.*)" dialog button is (enabled|disabled)$/, function(button, expectedState, callback) {
-    //    var isDisabled = (expectedState === 'disabled' ? 'true' : null);
-    //    browser.wait(function() {
-    //        return (dialog.getButton(button).getAttribute('disabled')
-    //            .then(function(disabled) {
-    //                return disabled === isDisabled;
-    //            }));
-    //    }, 2000)
-    //        .then(function() {
-    //            dialog.getButton(button).getAttribute('disabled').should.eventually.equal(isDisabled)
-    //        }).should.notify(callback);
-    //});
-    //
-    //this.Then(/^no dialog is displayed$/, function(callback) {
-    //    dialog.waitToDisappear().should.notify(callback);
-    //});
-    //
-    //this.Then(/^a dialog with title "(.*)" is not displayed$/, function(expectedTitle, callback) {
-    //    dialog.waitToAppear()
-    //        .then(function() {
-    //            dialog.getTitle().getText().should.not.equal(expectedTitle)
-    //        }).should.notify(callback);
-    //});
-    //
-    //this.Given(/^the dialog disappears$/, function(callback) {
-    //    dialog.waitToDisappear().should.notify(callback);
-    //});
-    //
-    //this.When(/^I click the "(.*)" dialog link$/, function(linkText, callback) {
-    //    dialog.get().element(by.cssContainingText('a', linkText)).click().should.notify(callback);
-    //});
-    //
-    //this.Then(/^the "(.*)" dialog link (is|is not) displayed$/, function(linkText, toggle, callback) {
-    //    var expectToBeDisplayed = (toggle === 'is' ? true : false);
-    //
-    //    if (expectToBeDisplayed) {
-    //        dialog.get().element(by.cssContainingText('a', linkText)).isDisplayed().should.eventually.be.true.should.notify(callback);
-    //    } else {
-    //        browser.wait(protractor.ExpectedConditions.invisibilityOf(dialog.get().element(by.cssContainingText('a', linkText))), 0).should.eventually.be.true.should.notify(callback);
-    //    }
-    //});
-    //
-    //this.Then(/^the "(.*)" dialog link is (enabled|disabled)$/, function(linkText, expectedState, callback) {
-    //    var isDisabled = (expectedState === 'disabled' ? 'true' : null);
-    //    browser.wait(function() {
-    //        return (dialog.get().element(by.cssContainingText('a', linkText)).getAttribute('disabled')
-    //            .then(function(disabled) {
-    //                return disabled === isDisabled;
-    //            }));
-    //    }, 2000)
-    //        .then(function() {
-    //            dialog.get().element(by.cssContainingText('a', linkText)).getAttribute('disabled').should.eventually.equal(isDisabled)
-    //        }).should.notify(callback);
-    //});
 };
