d7ef2f9ec169121fc429c964ecb7bc63f177caf6
sestov <vsesto@odecee.com.au>
2016-06-15 09:30:05 +1000

    Finialising the deployment script

--- firebase_deploy.sh
+++ firebase_deploy.sh
@@ -3,12 +3,14 @@
 TRAVIS_BRANCH=$1
 FIREBASE_PROJECT_VINCE=$2
 FIREBASE_TOKEN_VINCE=$3
+FIREBASE_PROJECT_PROD=$4
+FIREBASE_TOKEN_PROD=$5
 
 # Test the branch and deploy where needed
-if [[ ${TRAVIS_BRANCH} == *"develop"* ]]
+if [[ ${TRAVIS_BRANCH} == *"feature"* ]]
 then
 	echo "Feature branch does not get deployed to firebase"
-elif [[ ${TRAVIS_BRANCH} == *"feature"* ]]
+elif [[ ${TRAVIS_BRANCH} == *"develop"* ]]
 then
 	echo "Deploy to dev firebase environment"
 	firebase deploy --project "${FIREBASE_PROJECT_VINCE}" --token "${FIREBASE_TOKEN_VINCE}"
